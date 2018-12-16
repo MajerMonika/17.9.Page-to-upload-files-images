352768a7a526dfc3f1c219bd64a3e5f6178b2d00
Monika Majer <majer_monika@interia.pl>
2018-12-16 17:29:36 +0100

    Add npm formidable

--- modules/server.js
+++ modules/server.js
@@ -1,14 +1,28 @@
 var http = require('http');
 var colors = require('colors');
 
+var handlers = require('./handlers'); // nasz moduł
+
 function start() {
   function onRequest(request, response) {
-    console.log("Odebrano zapytanie.");
+    console.log("Odebrano zapytanie.".green);
+    console.log("Zapytanie " + request.url + " odebrane.");
+
     response.writeHead(200, {
-      "Content-Type": "text/plain"
+      "Content-Type": "text/plain; charset=utf-8"
     });
-    response.write("Pierwsze koty za płoty");
-    response.end();
+
+    switch (request.url) { // switch rozróżniający zapytania
+      case '/':
+      case '/start':
+        handlers.welcome(request, response);
+        break;
+      case '/upload':
+        handlers.upload(request, response);
+        break;
+      default:
+        handlers.error(request, response);
+    }
   }
 
   http.createServer(onRequest).listen(9000);
@@ -16,4 +30,4 @@ function start() {
   console.log("Uruchomiono serwer!".green);
 }
 
-exports.start = start
+exports.start = start;
